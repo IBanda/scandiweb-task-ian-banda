b5c6b3a6cdadb55aaa677852a306f16309bc4a73
Ian Banda <ian.d.banda@gmail.com>
2022-02-13 18:38:20 +0200

    add category reducer

--- src/store/reducers.ts
+++ src/store/reducers.ts
@@ -1,14 +1,29 @@
-import type { Action } from 'redux';
+import type { AnyAction } from 'redux';
+import actionTypes from './actionTypes';
 
 const initialState = {
      cart: [],
 };
 
-function cartReducer(state = initialState, action: Action) {
+function cartReducer(state = initialState, action: AnyAction) {
      switch (action.type) {
           default:
                return state;
      }
 }
 
-export default cartReducer;
+function categoryReducer(state = 'all', action: AnyAction) {
+     switch (action.type) {
+          case actionTypes.categoryChange:
+               return action.payload.category;
+          default:
+               return state;
+     }
+}
+
+const rootReducer = {
+     cart: cartReducer,
+     category: categoryReducer,
+};
+
+export default rootReducer;

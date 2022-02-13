be94001cdf908fd58455c4b1a2af69009f523434
Ian Banda <ian.d.banda@gmail.com>
2022-02-14 01:45:49 +0200

    add icons

---
+++ src/components/Icon.tsx
@@ -0,0 +1,43 @@
+import { Component } from 'react';
+
+export class LineAngleDownIcon extends Component {
+     render() {
+          return (
+               <svg
+                    version="1.1"
+                    id="Layer_1"
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlnsXlink="http://www.w3.org/1999/xlink"
+                    x="0px"
+                    y="0px"
+                    viewBox="0 0 122.88 66.91"
+                    xmlSpace="preserve"
+               >
+                    <g>
+                         <path d="M11.68,1.95C8.95-0.7,4.6-0.64,1.95,2.08c-2.65,2.72-2.59,7.08,0.13,9.73l54.79,53.13l4.8-4.93l-4.8,4.95 c2.74,2.65,7.1,2.58,9.75-0.15c0.08-0.08,0.15-0.16,0.22-0.24l53.95-52.76c2.73-2.65,2.79-7.01,0.14-9.73 c-2.65-2.72-7.01-2.79-9.73-0.13L61.65,50.41L11.68,1.95L11.68,1.95z" />
+                    </g>
+               </svg>
+          );
+     }
+}
+
+export class LineAngleUpIcon extends Component {
+     render() {
+          return (
+               <svg
+                    version="1.1"
+                    id="Layer_1"
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlnsXlink="http://www.w3.org/1999/xlink"
+                    x="0px"
+                    y="0px"
+                    viewBox="0 0 122.88 66.91"
+                    xmlSpace="preserve"
+               >
+                    <g>
+                         <path d="M11.68,64.96c-2.72,2.65-7.08,2.59-9.73-0.14c-2.65-2.72-2.59-7.08,0.13-9.73L56.87,1.97l4.8,4.93l-4.81-4.95 c2.74-2.65,7.1-2.58,9.76,0.15c0.08,0.08,0.15,0.16,0.23,0.24L120.8,55.1c2.72,2.65,2.78,7.01,0.13,9.73 c-2.65,2.72-7,2.78-9.73,0.14L61.65,16.5L11.68,64.96L11.68,64.96z" />
+                    </g>
+               </svg>
+          );
+     }
+}

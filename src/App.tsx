8881b0110ded994db6667601f0dea2a8d5942732
Ian Banda <ian.d.banda@gmail.com>
2022-02-14 01:47:21 +0200

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import Header from './components/Header';
 
 class App extends React.Component {
      render() {
-          return <div />;
+          return (
+               <div className="container">
+                    <Header />
+               </div>
+          );
      }
 }
 

59afaed4dc96ba6698c367ad59d87a44e901e6f6
Ian Banda <ian.d.banda@gmail.com>
2022-02-13 14:51:28 +0200

    add app interfaces

---
+++ src/utils/interfaces.ts
@@ -0,0 +1,39 @@
+export interface Product {
+     id: string;
+     name: string;
+     inStock: boolean;
+     gallery: string;
+     description: string;
+     category: string;
+     attributes: [AttributeSet];
+     prices: Price[];
+     brand: string;
+}
+
+interface Price {
+     currency: Currency;
+     amount: number;
+}
+
+interface Attribute {
+     displayValue: string;
+     value: string;
+     id: string;
+}
+
+interface AttributeSet {
+     id: string;
+     name: string;
+     type: string;
+     items: Attribute[];
+}
+
+interface Category {
+     name: string;
+     products: Product[];
+}
+
+interface Currency {
+     label: string;
+     symbol: string;
+}

7443705250012f545ebfd2d4a014c29d1a6e6a46
Ian Banda <ian.d.banda@gmail.com>
2022-02-14 23:37:04 +0200

    add app routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
 import Header from './components/Header';
+import CategoryPage from './pages/category';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+const StyledApp = styled.div`
+     margin-bottom: 2em;
+`;
 
 class App extends React.Component {
      render() {
           return (
-               <div className="container">
+               <StyledApp className="container">
                     <Header />
-               </div>
+                    <Router>
+                         <Routes>
+                              <Route element={<CategoryPage />} path="/" />
+                         </Routes>
+                    </Router>
+               </StyledApp>
           );
      }
 }

05aec7c544e08f93227126e71a45f7538c51c466
Ian Banda <ian.d.banda@gmail.com>
2022-02-18 09:57:46 +0200

    add cart page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
 import CategoryPage from './pages/category';
 import ProductPage from './pages/product';
+import CartPage from './pages/cart';
 
 const StyledApp = styled.div`
      margin-bottom: 2em;
@@ -13,11 +14,12 @@ class App extends React.Component {
      render() {
           return (
                <Router>
+                    <Header />
                     <StyledApp className="container">
-                         <Header />
                          <Switch>
                               <Route component={CategoryPage} path="/" exact />
                               <Route component={ProductPage} path="/product/:id" exact />
+                              <Route component={CartPage} path="/cart" exact />
                          </Switch>
                     </StyledApp>
                </Router>

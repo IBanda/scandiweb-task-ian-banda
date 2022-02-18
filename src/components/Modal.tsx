0ee8e41d2433d8c33bf2745e6a15048a68484d56
Ian Banda <ian.d.banda@gmail.com>
2022-02-18 09:49:22 +0200

    add modal

---
+++ src/components/Modal.tsx
@@ -0,0 +1,33 @@
+import { Component } from 'react';
+import { createPortal } from 'react-dom';
+import styled from 'styled-components';
+
+const StyledModal = styled.div`
+     position: fixed;
+     top: 0;
+     left: 0;
+     height: 100%;
+     width: 100%;
+     z-index: 2;
+     background-color: rgba(57, 55, 72, 0.22);
+`;
+
+const modalRoot = document.getElementById('modal');
+export class Modal extends Component {
+     element = document.createElement('div');
+
+     componentDidMount() {
+          modalRoot?.append(this.element);
+     }
+
+     componentWillUnmount() {
+          this.element.remove();
+     }
+
+     render() {
+          return createPortal(
+               <StyledModal>{this.props.children}</StyledModal>,
+               this.element
+          );
+     }
+}

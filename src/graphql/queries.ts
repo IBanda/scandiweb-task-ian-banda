f398b03b3ff94d7625b55c479b5ffc68ce61546c
Ian Banda <ian.d.banda@gmail.com>
2022-02-20 02:51:00 +0200

    update query

--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -1,7 +1,7 @@
 import { gql } from '@apollo/client';
 
 export const GET_CATEGORIES = gql`
-     query GetProducts {
+     query GetCategories {
           categories {
                name
           }

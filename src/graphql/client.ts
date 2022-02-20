6322afcae4f76c903f7aad716860f5acf170ca11
Ian Banda <ian.d.banda@gmail.com>
2022-02-20 02:50:23 +0200

    update attribute set cache id

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -2,7 +2,14 @@ import { InMemoryCache, ApolloClient } from '@apollo/client';
 
 const apolloClient = new ApolloClient({
      uri: 'http://localhost:4000/graphql',
-     cache: new InMemoryCache(),
+     cache: new InMemoryCache({
+          typePolicies: {
+               AttributeSet: {
+                    //specify custom id to avoid attribute sets being overriden
+                    keyFields: ['id', 'items', ['id']],
+               },
+          },
+     }),
 });
 
 export default apolloClient;

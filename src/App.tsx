1fea83b6c528ae5f2616afd0668e6f65cf928080
Ian Banda <ian.d.banda@gmail.com>
2022-02-22 00:59:19 +0200

    remove unused variable

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
 import CategoryPage from './pages/category';
 import ProductPage from './pages/product';
